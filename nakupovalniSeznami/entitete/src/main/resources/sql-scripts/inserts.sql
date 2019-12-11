4634125c404faf0a9b4b4a077e4cf8b8fb52636a
Blarc <jakob.malezic@gmail.com>
2019-12-11 09:51:59 +0100

    added healthCheck

--- nakupovalniSeznami/entitete/src/main/resources/sql-scripts/inserts.sql
+++ nakupovalniSeznami/entitete/src/main/resources/sql-scripts/inserts.sql
@@ -2,11 +2,13 @@ INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Janez', 'Nov
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Jan', 'Bajt', 'janBajt');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Franc', 'Demsar', 'francDemsar');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Jakob', 'Malezic', 'jakobMalezic');
-INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Jana', 'Štremfelj', 'janaŠtremfelj');
+INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Jana', 'Stremfelj', 'janaStremfelj');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Gregor', 'Malezic', 'gregorMalezic');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Peter', 'Malezic', 'peterMalezic');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Janez', 'Demsar', 'janezDemsar');
 INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Luka', 'Gril', 'lukaGril');
+INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Marija', 'Marolt', 'marija13');
+INSERT INTO users (id, name, last_name, username) VALUES (DEFAULT, 'Matej', 'Marolt', 'matejMarolt');
 
 INSERT INTO items (id, description, name) VALUES (1, 'Egg yolks and whole eggs store significant amounts of protein and choline, and are widely used in cookery.', 'Eggs');
 INSERT INTO items (id, description, name) VALUES (2, 'Milk is a nutrient-rich, white liquid food produced by the mammary glands of mammals.', 'Milk');
